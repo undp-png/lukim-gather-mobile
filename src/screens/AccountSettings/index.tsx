4c5b1221c4dc715513ff52717b2c0d4c001e8c4a
Dipendra Pathak <subaspathak62@gmail.com>
2022-03-08 10:29:30 +0545

    Settings screen

---
+++ src/screens/AccountSettings/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import MenuItem from 'components/MenuItem';
+
+import styles from './styles';
+
+const AccountSettings = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.menuWrapper}>
+                <MenuItem title="Email" label="example@mail.com" />
+                <MenuItem title="Password" label="Change password" />
+            </View>
+        </View>
+    );
+};
+
+export default AccountSettings;
