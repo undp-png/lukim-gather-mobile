5ed7311bf6bd99b4966ab8a8ab926c02ef2ab250
Dipendra Pathak <subaspathak62@gmail.com>
2022-04-28 10:16:38 +0545

    Create survey review

---
+++ src/components/SurveyReview/index.tsx
@@ -0,0 +1,64 @@
+import React, {useCallback} from 'react';
+import {Pressable, View} from 'react-native';
+import {Icon} from 'react-native-eva-icons';
+import Svg, {Path} from 'react-native-svg';
+
+import cs from '@rna/utils/cs';
+
+import styles from './styles';
+
+interface Props {
+    name: string;
+    activeReview: string;
+    onPress(emo: string): void;
+    icon?: string;
+}
+
+const SurveyReview: React.FC<Props> = ({name, activeReview, onPress, icon}) => {
+    const handlePress = useCallback(() => {
+        onPress(name);
+    }, [name, onPress]);
+
+    return (
+        <Pressable
+            style={cs(styles.feeelWrapper, [
+                styles.activeFeel,
+                name === activeReview,
+            ])}
+            onPress={handlePress}>
+            <View
+                style={cs(styles.checked, [
+                    styles.hide,
+                    name !== activeReview,
+                ])}>
+                <Icon
+                    name="checkmark-circle-2"
+                    height={18}
+                    width={18}
+                    fill={'#196297'}
+                />
+            </View>
+            {icon ? (
+                <Icon
+                    name={icon}
+                    height={25}
+                    width={25}
+                    fill={name === activeReview ? '#EC6D25' : '#70747E'}
+                />
+            ) : (
+                <Svg width="24" height="8" viewBox="0 0 19 7" fill="none">
+                    <Path
+                        d="M17.5725 0H1.47246C0.94779 0 0.522461 0.425329 0.522461 0.95V1.05C0.522461 1.57467 0.94779 2 1.47246 2H17.5725C18.0971 2 18.5225 1.57467 18.5225 1.05V0.95C18.5225 0.425329 18.0971 0 17.5725 0Z"
+                        fill={name === activeReview ? '#EC6D25' : '#70747E'}
+                    />
+                    <Path
+                        d="M17.5725 5H1.47246C0.94779 5 0.522461 5.42533 0.522461 5.95V6.05C0.522461 6.57467 0.94779 7 1.47246 7H17.5725C18.0971 7 18.5225 6.57467 18.5225 6.05V5.95C18.5225 5.42533 18.0971 5 17.5725 5Z"
+                        fill={name === activeReview ? '#EC6D25' : '#70747E'}
+                    />
+                </Svg>
+            )}
+        </Pressable>
+    );
+};
+
+export default SurveyReview;
---
+++ src/components/SurveyReview/styles.tsx
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+import COLORS from 'utils/colors';
+
+export default StyleSheet.create({
+    feeelWrapper: {
+        position: 'relative',
+        height: 56,
+        width: '32%',
+        borderWidth: 1,
+        borderColor: COLORS.border,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    activeFeel: {
+        borderColor: COLORS.secondary,
+    },
+    checked: {
+        position: 'absolute',
+        top: 3,
+        left: 3,
+    },
+    hide: {
+        display: 'none',
+    },
+});
